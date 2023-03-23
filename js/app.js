b38d21ed0228fcb350926031c05f5ca1c70ad155
Rin Kates <erinkates11@gmail.com>
2023-03-23 17:38:23 -0400

    calculator working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
 
+// grab the display and button elements from HTML
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
+
+// give buttons commands
+buttons.forEach((button) => {
+  button.onclick = () => {
+
+    // clear button
+    if (button.id == "clear") {
+      display.innerText = "";
+    } 
+    // eval JS string and execute when = sign is pressed and values are present
+    else if (display.innerText != "" && button.id == "equal") {
+        display.innerText = eval(display.innerText);
+    }
+    // display null if no values in string
+    else if (display.innerText == "" && button.id == "equal") {
+      display.innerText = "null";
+    
+    // display button values
+    } else {
+      display.innerText += button.id;
+    }
+  };
+});
 
 
 
